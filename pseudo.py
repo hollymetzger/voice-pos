6f294dcb3d401fd16ac8ec573f9f8ac6fd1adb0b
Holly Metzger <hollymmetzger@gmail.com>
2023-08-17 19:18:21 -0400

    testing, added pseudo.py

---
+++ pseudo.py
@@ -0,0 +1,7 @@
+def get_response():
+    # get the users audo response and return it as a string
+    return
+
+def gpt_parse_response(response_str):
+    # use gpt to figure out what the user is saying and write a response
+    return
